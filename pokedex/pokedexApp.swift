dec5882ff53af7cc2695d0b2ebb50f0db0cb7bed
ZiXyos <killian.moudery@gmail.com>
2023-03-22 00:40:27 +0900

    Initial Commit

---
+++ pokedex/pokedexApp.swift
@@ -0,0 +1,17 @@
+//
+//  pokedexApp.swift
+//  pokedex
+//
+//  Created by Ktchoumh on 3/22/23.
+//
+
+import SwiftUI
+
+@main
+struct pokedexApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
