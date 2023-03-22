fbff4e68db5bd6a3faca0f9d98cdac2e77dd0950
ZiXyos <killian.moudery@gmail.com>
2023-03-22 16:09:42 +0900

    feat(auth-flow): add state manager

---
+++ pokeday/states/managers/app.state.manager.swift
@@ -0,0 +1,47 @@
+//
+//  app.state.manager.swift
+//  pokeday
+//
+//  Created by Ktchoumh on 3/22/23.
+//
+
+import Foundation;
+
+enum UserDefaultsKeys: String {
+	
+	case isAuth
+	case token
+	
+	func defaultKey() -> String {
+		return self.rawValue;
+	}
+}
+
+protocol AppStateManager_P {
+	
+	func getDefault(_ key: UserDefaultsKeys) -> Bool;
+	func setDefault(_ key: UserDefaultsKeys, value: Bool);
+
+	func getDefault(_ key: UserDefaultsKeys) -> String?;
+	func setDefault(_ key: UserDefaultsKeys, value: String);
+}
+
+struct AppStateManager: AppStateManager_P {
+	
+	
+	func getDefault(_ key: UserDefaultsKeys) -> Bool {
+		return UserDefaults.standard.bool(forKey: key.defaultKey());
+	}
+	
+	func setDefault(_ key: UserDefaultsKeys, value: Bool) {
+		UserDefaults.standard.set(value, forKey: key.defaultKey());
+	}
+	
+	func getDefault(_ key: UserDefaultsKeys) -> String? {
+		return UserDefaults.standard.string(forKey: key.defaultKey());
+	}
+	
+	func setDefault(_ key: UserDefaultsKeys, value: String) {
+		UserDefaults.standard.set(value, forKey: key.defaultKey());
+	}
+}
