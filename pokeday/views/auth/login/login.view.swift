4b3184a3bfdc27f0b90e74da4cc35397dcf8782e
ZiXyos <killian.moudery@gmail.com>
2023-03-22 15:05:55 +0900

    feat(auth): add navigation logic

---
+++ pokeday/views/auth/login/login.view.swift
@@ -0,0 +1,50 @@
+//
+//  login.view.swift
+//  pokeday
+//
+//  Created by Ktchoumh on 3/22/23.
+//
+
+import SwiftUI
+
+struct LoginView: View {
+	
+	@StateObject var viewModel: LoginViewModel
+	
+	@State var username: String = "";
+	@State var password: String = "";
+	
+    var body: some View {
+		VStack {
+			Text("This is the login page");
+			TextField("Username", text: self.$username)
+				.padding()
+				.background(Color.gray)
+				.cornerRadius(5.0)
+				.padding(.bottom, 20)
+	
+			SecureField("Password", text: self.$password)
+				.padding()
+				.background(Color.gray)
+				.cornerRadius(5.0)
+				.padding(.bottom, 20)
+			
+			Button("Login") {
+				Task {
+					self.viewModel.loginWithUsername(
+						username: self.username,
+						password: self.password
+					)
+				}
+			}
+		}
+    }
+}
+
+struct login_view_Previews: PreviewProvider {
+    static var previews: some View {
+        LoginView(
+			viewModel: LoginViewModel()
+		);
+    }
+}
